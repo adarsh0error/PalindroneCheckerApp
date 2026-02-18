0e7161b411f705dc4bf7748f61e738a1a48a5ac9
adarsh0error <adarshmittal621@gmail.com>
2026-02-18 15:07:28 +0530

    adarsh added logic for palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,25 @@
+//version 2.0
+//author Adarsh
+//useCase1: Welcome Page
+//useCase 2: hard code palindrome logic
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main() {
         System.out.println("welcome to palindrome checker app management system");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string");
+        String input = sc.nextLine();
+        String reversed = "";
+        for(int i = input.length()-1; i>=0; i++){
+            reversed+=input.charAt(i);
+        }
+        if(input.equals(reversed)){
+            System.out.println("is palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+        sc.close();
     }
 }
 
